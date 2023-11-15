bba2d110c9592b92e56149730bb662af5ecc71c8
Matiss Cukurs <cukursmatiss@gmail.com>
2023-11-15 20:42:46 +0200

    finish hashset basic functions

--- SetInterface/src/com/chukurs/Contact.java
+++ SetInterface/src/com/chukurs/Contact.java
@@ -9,6 +9,28 @@ public class Contact {
     Set<String> phones = new HashSet<>();
 
 
+    public void addEmail(String companyName) {
+        String[] names = name.split(" ");
+        String email = "%c%s@%s.com".formatted(
+                name.charAt(0),
+                names[names.length - 1],
+                companyName.replaceAll(" ", "").toLowerCase());
+        if (!emails.add(email)) {
+            System.out.println(email + " email already in the set\n");
+        } else {
+            System.out.println(email + " added to the set\n");
+        }
+
+    }
+
+    public void replaceEmailIfExists(String oldEmail, String newEmail) {
+        //hashSet does not include replace method, so this is a workaround
+        if (emails.contains(oldEmail)) {
+            emails.remove(oldEmail);
+            emails.add(newEmail);
+        }
+    }
+
     public Contact(String name) {
         this(name, null);
     }
@@ -20,6 +42,21 @@ public class Contact {
 
     public Contact(String name, long phone) {
         this(name, null, phone);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        return name.equals(contact.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return 33 * name.hashCode();
     }
 
     public Contact(String name, String email, long phone) {
@@ -52,7 +89,6 @@ public class Contact {
         newContact.phones = new HashSet<>(this.phones);
         newContact.emails.addAll(contact.emails);
         newContact.phones.addAll(contact.phones);
-
 
 
         return newContact;
--- SetInterface/src/com/chukurs/Main.java
+++ SetInterface/src/com/chukurs/Main.java
@@ -1,7 +1,9 @@
 package com.chukurs;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +11,29 @@ public class Main {
         printData("email data", emails);
         List<Contact> phones = ContactData.getData("phone");
         printData("phone data", phones);
+        System.out.println("-".repeat(10) + " NOW moving from Lists to Sets");
+        //Crate hashSets from the Lists created by reading the String (can be created this way as both ar form of Collection)
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+        /*
+        printData method takes any Collection,
+        looks like duplicates are present in HashSet but that is not true. They are not duplicates as Hash is different
+        + .equal() is false
+        */
+        printData("Email contacts", emailContacts);
+        printData("Phone contacts", phoneContacts);
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood forest");
+        //test if we can add a duplicate
+        robinHood.addEmail("Sherwood forest");
+        //test the replace method
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.net");
+        System.out.println(robinHood);
+
     }
-    public static void printData(String header, Collection<Contact> contacts){
+
+    public static void printData(String header, Collection<Contact> contacts) {
         System.out.println("-".repeat(10));
         System.out.println(header);
         System.out.println("-".repeat(10));
